514c5540fc362447da5705aad80ae38f471090f0
acaiyudai <ispectrene@outlook.com>
2022-10-04 20:35:59 +0900

    an introduction of Kang Seulgi.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import './assets/styles/style.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Kang  Seulgi</h1>
+      <p>Kang Seulgi is a menber of Red</p>
+      <p>She is 30 years old.</p>
+      <p>She commits us to wonderful experience.</p>
+
     </div>
   );
 }
